a8b300cf95514e838cf1e63706c61357f85eb940
Kadin Zhang <cykadin@gmail.com>
2019-07-23 13:27:04 -0400

    Install dependency, follow docs

--- shared/redux/store/index.tsx
+++ shared/redux/store/index.tsx
@@ -3,14 +3,26 @@
 import { applyMiddleware, createStore } from 'redux';
 import * as thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import reducers from '../reducers';
 
+const persistConfig = {
+	key: 'root',
+	storage: AsyncStorage,
+};
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 let middlewares = [thunkMiddleware.default];
 if (__DEV__) {
-  const loggerMiddleware = createLogger();
-  middlewares = [...middlewares, loggerMiddleware];
+	const loggerMiddleware = createLogger();
+	middlewares = [...middlewares, loggerMiddleware];
 }
-const store = createStore(reducers, applyMiddleware(...middlewares));
 
-export default store;
+export default () => {
+	let store = createStore(persistedReducer, applyMiddleware(...middlewares));
+	let persistor = persistStore(store);
+	return { store, persistor };
+};
+
