004bfacdcbb436dfbcee3cdf17c7b775a0babf04
Kadin Zhang <cykadin@gmail.com>
2019-07-29 14:38:58 -0400

    Lock android orientation to portrait, add chat waiting screen

--- src/view/forms/SignIn/index.tsx
+++ src/view/forms/SignIn/index.tsx
@@ -1,54 +1,42 @@
-import React, { FunctionComponent } from 'react'
-import { Icon } from 'native-base'
-import { Formik } from 'formik'
-import { Button, Form, FormItem, Input } from '../../components'
-import { Hint } from './styled'
-import { getValidationSchema, initialValues, isEmailError } from './data'
-import { IProps } from './types'
+import React, { FunctionComponent } from 'react';
+import { Icon } from 'native-base';
+import { Formik } from 'formik';
+import { Button, Form, FormItem, Input } from '../../components';
+import { Hint } from './styled';
+import { getValidationSchema, initialValues, isEmailError } from './data';
+import { IProps } from './types';
 
 const SignInForm: FunctionComponent<IProps> = ({ onSubmit }) => (
-  <Formik
-    initialValues={initialValues}
-    validationSchema={getValidationSchema}
-    onSubmit={onSubmit}
-  >
-    {({
-      handleChange,
-      handleSubmit,
-      values: { email, password },
-      isSubmitting,
-      touched,
-      errors,
-    }) => (
-      <Form style={{justifyContent: "center"}}>
-        <FormItem error={isEmailError(touched, errors)}>
-          <Input
-            placeholder="Email"
-            onChangeText={handleChange('email')}
-            value={email}
-            autoCapitalize="none"
-            autoCorrect={false}
-          />
-          
-          {isEmailError(touched, errors) && <Icon name="close-circle" />}
-        </FormItem>
-        <FormItem>
-          <Input
-            placeholder="Password"
-            onChangeText={handleChange('password')}
-            value={password}
-            autoCapitalize="none"
-            autoCorrect={false}
-            secureTextEntry
-          />
-          <Hint>hint: password</Hint>
-        </FormItem>
-        <Button disabled={isSubmitting} onPress={handleSubmit} width={'100%'} block>
-          {isSubmitting ? 'loading...' : 'Sign In'}
-        </Button>
-      </Form>
-    )}
-  </Formik>
-)
+	<Formik initialValues={initialValues} validationSchema={getValidationSchema} onSubmit={onSubmit}>
+		{({ handleChange, handleSubmit, values: { email, password }, isSubmitting, touched, errors }) => (
+			<Form style={{ justifyContent: 'center' }}>
+				<FormItem error={isEmailError(touched, errors)}>
+					<Input
+						placeholder="Email"
+						onChangeText={handleChange('email')}
+						value={email}
+						autoCapitalize="none"
+						autoCorrect={false}
+					/>
 
-export default SignInForm
+					{isEmailError(touched, errors) && <Icon name="close-circle" />}
+				</FormItem>
+				<FormItem>
+					<Input
+						placeholder="Password"
+						onChangeText={handleChange('password')}
+						value={password}
+						autoCapitalize="none"
+						autoCorrect={false}
+						secureTextEntry
+					/>
+				</FormItem>
+				<Button disabled={isSubmitting} onPress={handleSubmit} width={'100%'} block>
+					{isSubmitting ? 'loading...' : 'Sign In'}
+				</Button>
+			</Form>
+		)}
+	</Formik>
+);
+
+export default SignInForm;
--- src/view/screens/Session/Component.tsx
+++ src/view/screens/Session/Component.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
 import {} from 'native-base';
 import { H1, Text } from '../../components/Text';
 import TopBar from '../TopBar';
@@ -20,15 +20,40 @@ export default class Session extends React.Component<Props, State> {
 	}
 
 	componentDidMount() {
-		socket.emit('hello');
+		socket.emit('join');
 	}
 
 	render() {
 		return (
 			<View>
-				<TopBar color={'#FFF'} chat />
+				<TopBar color={'#fff'} chat />
 				<View style={styles.container}>
-					<Text>{this.props.sessionId}</Text>
+					{/* <Text>{this.props.sessionId}</Text> */}
+					<View style={[styles.tip, { backgroundColor: '#16D2AA' }]}>
+						<Image style={styles.image} source={require('../../assets/images/bulb.png')} />
+						<H1 color={'#fff'}>We’re looking for a coach for you...</H1>
+						<Text color={'#fff'} light>
+							Thanks for requesting help with UPchieve! We’re looking for a coach to pair with you now.
+							This process typically takes 5-10 minutes.
+						</Text>
+					</View>
+					<View style={[styles.tip, { backgroundColor: '#1855D1' }]}>
+						<H1 color={'#fff'}>While you wait...</H1>
+						<Text color={'#fff'} light>
+							While you’re waiting, you can write any problems you’re working on on the whiteboard. (Click
+							on the pencil icon in the upper right corner of the screen to start.)
+						</Text>
+					</View>
+					<View style={[styles.tip, { backgroundColor: '#FF8C5F' }]}>
+						<H1 color={'#fff'}>A couple of reminders:</H1>
+						<Text color={'#fff'} light>
+							1. Practice online safety! Don’t share personal information like your phone number or email.
+						</Text>
+						<Text color={'#fff'} light>
+							2. Click “End chat” when you’re done, and make sure to fill out the short feedback form that
+							follows!
+						</Text>
+					</View>
 				</View>
 			</View>
 		);
@@ -39,7 +64,20 @@ const styles = {
 	container: {
 		marginTop: 80,
 		backgroundColor: '#fff',
-		flex: 1,
 		width: '100%',
+	},
+	image: {
+		width: 32,
+		height: 48,
+		flex: 1,
+		resizeMode: 'contain',
+		// alignSelf: 'flex-start',
+	},
+	tip: {
+		width: '90%',
+		alignSelf: 'center',
+		borderRadius: 8,
+		margin: 8,
+		padding: 16,
 	},
 };
